c7a2e574c45ec64638dafc616a71a8b08ab19652
LAtashini <148990352+LAtashini@users.noreply.github.com>
2025-05-30 13:10:46 +0530

    Update multer.js

--- Backend/middlewares/multer.js
+++ Backend/middlewares/multer.js
@@ -1,18 +1,18 @@
 import multer from 'multer';
 import path from 'path';
 
-// Use import.meta.url to get the current directory in ES modules
+
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 const uploadDir = path.join(__dirname, 'uploads');
 
-// Configure multer to store files in the uploads directory
+
 const storage = multer.diskStorage({
     destination: function(req, file, callback) {
-        callback(null, uploadDir);  // specify the directory where files will be stored
+        callback(null, uploadDir);  
     },
     filename: function(req, file, callback) {
-        callback(null, file.originalname);  // use the original file name
+        callback(null, file.originalname);  
     }
 });
 
